ab17a6549b153e5f3a3a7714f7b26ff63d48faad
ekaramustafa20 <ekaramustafa20@ku.edu.tr>
2024-05-21 00:27:30 +0200

    enemy is added

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 1f;
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+
+    private float direction;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        direction = 1f;
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(movementSpeed, 0f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        movementSpeed = -movementSpeed;
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), 1f);
+    }
+}
