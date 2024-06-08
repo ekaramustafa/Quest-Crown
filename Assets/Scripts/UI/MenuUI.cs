046446160ff3187badb836e008c9fdeb3d033a90
ekaramustafa20 <ekaramustafa20@ku.edu.tr>
2024-06-08 21:38:14 +0200

    logic is implemented

---
+++ Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,24 @@
+using CodeMonkey.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour
+{
+    [SerializeField] private Transform startButton;
+    [SerializeField] private Transform exitButton;
+
+
+
+    private void Awake()
+    {
+        startButton.GetComponent<Button_UI>().ClickFunc = () => {
+            Loader.Load(Loader.Scene.GameScene);
+        };
+
+        exitButton.GetComponent<Button_UI>().ClickFunc = () => {
+            Application.Quit();
+        };
+    }
+}
